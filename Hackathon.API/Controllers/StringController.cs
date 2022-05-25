43c96a1730ded533c53ffc51a7836368e517c2d5
Andy Prior <Andy.Prior@nationwide.co.uk>
2022-05-25 16:36:25 +0100

    Add API code

---
+++ Hackathon.API/Controllers/StringController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon.API.Controllers
+{
+    public class StringController : Controller
+    {
+        public string ReverseString(string inputString)
+        {
+            string reversed = new(inputString.Reverse().ToArray());
+            return reversed;
+        }
+    }
+}
