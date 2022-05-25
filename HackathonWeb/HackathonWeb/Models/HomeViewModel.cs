abcda128ff836f5e4971605bab59469168994da6
Duncan Leung <Duncan.Leung@nationwide.co.uk>
2022-05-25 18:02:49 +0100

    Hackathon Web

---
+++ HackathonWeb/HackathonWeb/Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HackathonWeb.Models
+{
+    public class HomeViewModel
+    {
+        public string Weather { get; set; }
+        public string Input { get; set; }
+        public string ReversedString { get; set; }
+        public IEnumerable<WeatherForecast> WeatherForecastList { get; set; }
+    }
+}
